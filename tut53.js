c4510d60a8fa241c745ad5f7ce703cdeb6588e64
Asutosh Sahoo <sasutosh71@gmail.com>
2021-11-20 00:02:59 +0530

    tut53

--- tut53.js
+++ tut53.js
@@ -1,1 +1,25 @@
 console.log("This is tutorial 53");
+
+ let name = "munu";
+ let name2 = "Asu";
+ let name3 = "fiz";
+ let greetText = "goodnight";
+ function greet(name , greeting = "Greetings from JavaScript"){
+     console.log(name + " " + greeting);
+ }
+//  greet(name , greetText);
+//  greet(name2 , greetText);
+//    let returnValue = greet(name3);
+//    console.log(returnValue);
+// console.log(name + " is a good boy");
+// console.log(name2 + " is a good boy");
+// console.log(name3 + " is a good boy");
+
+function sum( a, b , c)
+{
+    let d = a+b+c;
+    return d;
+}
+ let returnValue = sum(1,2,3);
+ console.log(returnValue);
+
